b91ebb2959ea53fdeee40d8ceb906d3fc6a4b028
Dmitrii Tarasov <d.tarasov@airi.net>
2025-07-30 13:16:07 +0300

    any fixes new dataset hf

--- Data/mathbridge/mathbridge_push_to_hub.py
+++ Data/mathbridge/mathbridge_push_to_hub.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
 
     if not os.path.exists(dataset_path):
 
-        df = pd.read_csv('./MathBridge_train_cleaned_normalized_train.csv')
+        df = pd.read_csv('./mb_whisper.csv')
 
         dataset = datasets.Dataset.from_pandas(df)
 
@@ -19,15 +19,26 @@ if __name__ == "__main__":
 
         wav_files = set(map(lambda x: f'./mathbridge_cleaned_tts_wavs/{x}', os.listdir('./mathbridge_cleaned_tts_wavs')))
 
-        dataset = dataset.map(lambda x: { 'audio_path': "./mathbridge_cleaned_tts_wavs/" + str(x['__index_level_0__']) + '.wav' }, batched=False)
-        dataset = dataset.remove_columns(['__index_level_0__'])
+        dataset = dataset.rename_columns({
+            'equation': 'sentence',
+            'spoken_English': 'pronunciation',
+            'formula_normalized': 'sentence_normalized'
+        })
+
+        dataset = dataset.map(lambda x: {
+            'audio_path': "./mathbridge_cleaned_tts_wavs/" + str(x['index']) + '.wav',
+            'sentence': x['sentence'].removeprefix('$ ').removesuffix(' $'),
+            'sentence_normalized': x['sentence_normalized'].removeprefix('$ ').removesuffix(' $'),
+        }, batched=False)
+        dataset = dataset.remove_columns(['__index_level_0__', 'index'])
         dataset_filtered = dataset.filter(lambda x: x['audio_path'] in wav_files)
 
         print('len(dataset)', len(dataset_filtered))
+        assert len(wav_files) == len(dataset_filtered)
 
         dataset_filtered = dataset_filtered.cast_column('audio_path', datasets.Audio(sampling_rate=16000))
 
-        dataset_filtered.save_to_disk(dataset_path)
+        dataset_filtered.save_to_disk(dataset_path, num_proc=32)
     else:
         dataset_filtered = datasets.Dataset.load_from_disk(dataset_path)
 
