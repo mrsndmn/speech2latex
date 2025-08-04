a7628f3e98263ef9aeae073bccd38030aa86a87d
Dmitrii Tarasov <d.tarasov@airi.net>
2025-08-04 19:32:59 +0300

    fix sample datasets

--- sample_datasets.py
+++ sample_datasets.py
@@ -1,3 +1,4 @@
+import torch
 import torchaudio
 
 import os
@@ -17,21 +18,24 @@ if __name__ == "__main__":
         shuffled_dataset = dataset_dict[key].shuffle(seed=42).select(range(NUM_SAMPLES))
         for i, item in enumerate(shuffled_dataset):
             audio_path = item['audio_path']['array']
-            sample_rate = item['audio_path']['sample_rate']
+            sampling_rate = item['audio_path']['sampling_rate']
 
-            torchaudio.save(f'./sample_datasets/{key}/{i:02d}.wav', audio_path, sample_rate)
+            torchaudio.save(f'./sample_datasets/{key}/{i:02d}.wav', torch.from_numpy(audio_path).unsqueeze(0), sampling_rate)
 
 
     # MathBridge Cleaned Subset Data
     dataset_dict = load_dataset('marsianin500/Speech2LatexMathBridge', num_proc=32)
 
     for key in dataset_dict.keys():
+        os.makedirs(f'./sample_datasets/{key}', exist_ok=True)
         shuffled_dataset = dataset_dict[key].shuffle(seed=42).select(range(NUM_SAMPLES))
-        for i, item in enumerate(shuffled_dataset):
-            audio_path = item['audio_path']['array']
-            sample_rate = item['audio_path']['sample_rate']
 
-            torchaudio.save(f'./sample_datasets/{key}/{i:02d}.wav', audio_path, sample_rate)
+        key = f'mathbridge_{key}'
+        for i, item in enumerate(shuffled_dataset):
+            audio_array = item['audio_path']['array']
+            sampling_rate = item['audio_path']['sampling_rate']
+
+            torchaudio.save(f'./sample_datasets/{key}/{i:02d}.wav', torch.from_numpy(audio_array).unsqueeze(0), sampling_rate)
 
 
 
