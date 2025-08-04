792ab1d0f354b667f032bed74e14480523406c80
Dmitrii Tarasov <d.tarasov@airi.net>
2025-08-04 21:46:55 +0300

    wip sample datasets

--- sample_datasets.py
+++ sample_datasets.py
@@ -6,7 +6,7 @@ from datasets import load_dataset, Audio, Dataset
 
 if __name__ == "__main__":
 
-    NUM_SAMPLES = 5
+    NUM_SAMPLES = 10
 
     # Speech2Latex Equations and Sentences
     os.makedirs('./sample_datasets', exist_ok=True)
@@ -15,33 +15,37 @@ if __name__ == "__main__":
 
     for key in dataset_dict.keys():
         os.makedirs(f'./sample_datasets/{key}', exist_ok=True)
-        shuffled_dataset = dataset_dict[key].shuffle(seed=42).select(range(1000))
-        shuffled_dataset_tts = shuffled_dataset.filter(lambda x: int(x['is_tts']) == 1)
-        shuffled_dataset_human = shuffled_dataset.filter(lambda x: int(x['is_tts']) == 0)
 
-        for i, item in enumerate(shuffled_dataset_tts):
-            audio_path = item['audio_path']['array']
-            sampling_rate = item['audio_path']['sampling_rate']
+        for language in [ 'eng', 'ru' ]:
 
-            torchaudio.save(f'./sample_datasets/{key}/tts_{i:02d}.wav', torch.from_numpy(audio_path).unsqueeze(0), sampling_rate)
+            shuffled_dataset = dataset_dict[key].shuffle(seed=42).select(range(10000))
+            shuffled_dataset = shuffled_dataset.filter(lambda x: x['language'] == language)
+            shuffled_dataset_tts = shuffled_dataset.filter(lambda x: int(x['is_tts']) == 1)
+            shuffled_dataset_human = shuffled_dataset.filter(lambda x: int(x['is_tts']) == 0)
 
-        for i, item in enumerate(shuffled_dataset_human):
-            audio_path = item['audio_path']['array']
-            sampling_rate = item['audio_path']['sampling_rate']
+            for i, item in enumerate(shuffled_dataset_tts):
+                audio_path = item['audio_path']['array']
+                sampling_rate = item['audio_path']['sampling_rate']
 
-            torchaudio.save(f'./sample_datasets/{key}/human_{i:02d}.wav', torch.from_numpy(audio_path).unsqueeze(0), sampling_rate)
+                torchaudio.save(f'./sample_datasets/{key}/tts_{language}_{i:02d}.wav', torch.from_numpy(audio_path).unsqueeze(0), sampling_rate)
+
+            for i, item in enumerate(shuffled_dataset_human):
+                audio_path = item['audio_path']['array']
+                sampling_rate = item['audio_path']['sampling_rate']
+
+                torchaudio.save(f'./sample_datasets/{key}/human_{language}_{i:02d}.wav', torch.from_numpy(audio_path).unsqueeze(0), sampling_rate)
 
 
-    # # MathBridge Cleaned Subset Data
-    # dataset_dict = load_dataset('marsianin500/Speech2LatexMathBridge', num_proc=32)
+        # # MathBridge Cleaned Subset Data
+        # dataset_dict = load_dataset('marsianin500/Speech2LatexMathBridge', num_proc=32)
 
-    # for key in dataset_dict.keys():
-    #     shuffled_dataset = dataset_dict[key].shuffle(seed=42).select(range(NUM_SAMPLES))
+        # for key in dataset_dict.keys():
+        #     shuffled_dataset = dataset_dict[key].shuffle(seed=42).select(range(NUM_SAMPLES))
 
-    #     key = f'mathbridge_{key}'
-    #     os.makedirs(f'./sample_datasets/{key}', exist_ok=True)
-    #     for i, item in enumerate(shuffled_dataset):
-    #         audio_array = item['audio_path']['array']
-    #         sampling_rate = item['audio_path']['sampling_rate']
+        #     key = f'mathbridge_{key}'
+        #     os.makedirs(f'./sample_datasets/{key}', exist_ok=True)
+        #     for i, item in enumerate(shuffled_dataset):
+        #         audio_array = item['audio_path']['array']
+        #         sampling_rate = item['audio_path']['sampling_rate']
 
-    #         torchaudio.save(f'./sample_datasets/{key}/{i:02d}.wav', torch.from_numpy(audio_array).unsqueeze(0), sampling_rate)
+        #         torchaudio.save(f'./sample_datasets/{key}/{i:02d}.wav', torch.from_numpy(audio_array).unsqueeze(0), sampling_rate)
