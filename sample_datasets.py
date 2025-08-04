497d7ccce63d099873f719f416e91f285bd67a29
Dmitrii Tarasov <d.tarasov@airi.net>
2025-08-04 21:14:14 +0300

    fix sample datasets

--- sample_datasets.py
+++ sample_datasets.py
@@ -27,15 +27,12 @@ if __name__ == "__main__":
     dataset_dict = load_dataset('marsianin500/Speech2LatexMathBridge', num_proc=32)
 
     for key in dataset_dict.keys():
-        os.makedirs(f'./sample_datasets/{key}', exist_ok=True)
         shuffled_dataset = dataset_dict[key].shuffle(seed=42).select(range(NUM_SAMPLES))
 
         key = f'mathbridge_{key}'
+        os.makedirs(f'./sample_datasets/{key}', exist_ok=True)
         for i, item in enumerate(shuffled_dataset):
             audio_array = item['audio_path']['array']
             sampling_rate = item['audio_path']['sampling_rate']
 
             torchaudio.save(f'./sample_datasets/{key}/{i:02d}.wav', torch.from_numpy(audio_array).unsqueeze(0), sampling_rate)
-
-
-
